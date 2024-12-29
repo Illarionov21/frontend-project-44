6ff3fa6418bf6f03e90147c2cf10d42be2487e23
Nikita <testemail@mail.ru>
2024-12-29 15:14:46 +0300

    Rewrote the code for aggregation

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export function userName() {
     var name = readlineSync.question('May I have your name? ');
-    console.log('Hi ' + name + '!');
+    console.log(`Hello, ${name}!`);
 }
