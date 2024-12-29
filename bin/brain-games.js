314ba32dc5e1abffa577869fb9b81d15fb6db996
Nikita <testemail@mail.ru>
2024-12-29 15:09:23 +0300

    Modified brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
-<<<<<<< HEAD
 import { userName } from "../src/cli.js";
 
-=======
->>>>>>> 82ded960bd92bbe1e0e5f581501ea1e49232b5cc
 console.log('Welcome to the Brain Games!');
 
 userName();
