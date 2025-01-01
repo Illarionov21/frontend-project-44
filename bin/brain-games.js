5ca2b4c9b3fb601f6909a361b1c1d76afbff2c4c
Nikita <testemail@mail.ru>
2025-01-01 23:23:46 +0300

    Update file

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
 import userName from '../src/cli.js';
+import brainEven from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
 
-userName();
+const name = userName();
+console.log(`Hello, ${name}!`);
+
+brainEven(name);
