22198211aa52c6b0e5530ff94d51184e488749f8
Nikita <testemail@mail.ru>
2025-01-01 23:22:46 +0300

    Modified file

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default function userName() {
   const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  return name;
 }
