7a68ee8cd495d73bbaa150f9bb47ded040b6fd5b
Nikita <testemail@mail.ru>
2025-01-08 00:08:23 +0300

    added game file

---
+++ src/games/even.js
@@ -0,0 +1,16 @@
+import runGameEngine from '../index.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const isEven = (number) => number % 2 === 0;
+
+const generateRound = () => {
+    const randomNumber = Math.floor(Math.random() * 101);
+    const question = randomNumber;
+    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+    return [question, correctAnswer];
+}
+
+const startEvenGame = () => runGameEngine(description, generateRound);
+
+export default startEvenGame;
