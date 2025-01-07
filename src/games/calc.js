57918de331eecb4a7daf481d34259ae609051305
Nikita <testemail@mail.ru>
2025-01-08 00:08:15 +0300

    added game file

---
+++ src/games/calc.js
@@ -0,0 +1,35 @@
+import runGameEngine from '../index.js';
+
+const description = 'What is the result of the expression?';
+
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateRound = () => {
+    const num1 = getRandomNumber(1, 50);
+    const num2 = getRandomNumber(1, 50);
+
+    const operators = ['+', '-', '*'];
+    const operator = operators[getRandomNumber(0, operators.length - 1)];
+
+    const question = `${num1} ${operator} ${num2}`;
+
+    let correctAnswer;
+    switch (operator) {
+        case '+':
+            correctAnswer = num1 + num2;
+            break;
+        case '-':
+            correctAnswer = num1 - num2;
+            break;
+        case '*':
+            correctAnswer = num1 * num2;
+            break;
+        default:
+            throw new Error(`Unsupported operator: ${operator}`);
+    }
+    return [question, correctAnswer.toString()];
+}
+
+const startCalcGame = () => runGameEngine(description, generateRound);
+
+export default startCalcGame;
