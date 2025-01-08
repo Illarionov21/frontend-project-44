a970d7d0b138b997f1ff27e9a79a2ef790e1be08
Nikita <testemail@mail.ru>
2025-01-08 20:34:27 +0300

    EsLint changes

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,30 +5,30 @@ const description = 'What is the result of the expression?';
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const generateRound = () => {
-    const num1 = getRandomNumber(1, 50);
-    const num2 = getRandomNumber(1, 50);
+  const num1 = getRandomNumber(1, 50);
+  const num2 = getRandomNumber(1, 50);
 
-    const operators = ['+', '-', '*'];
-    const operator = operators[getRandomNumber(0, operators.length - 1)];
+  const operators = ['+', '-', '*'];
+  const operator = operators[getRandomNumber(0, operators.length - 1)];
 
-    const question = `${num1} ${operator} ${num2}`;
+  const question = `${num1} ${operator} ${num2}`;
 
-    let correctAnswer;
-    switch (operator) {
-        case '+':
-            correctAnswer = num1 + num2;
-            break;
-        case '-':
-            correctAnswer = num1 - num2;
-            break;
-        case '*':
-            correctAnswer = num1 * num2;
-            break;
-        default:
-            throw new Error(`Unsupported operator: ${operator}`);
-    }
-    return [question, correctAnswer.toString()];
-}
+  let correctAnswer;
+  switch (operator) {
+    case '+':
+      correctAnswer = num1 + num2;
+      break;
+    case '-':
+      correctAnswer = num1 - num2;
+      break;
+    case '*':
+      correctAnswer = num1 * num2;
+      break;
+    default:
+      throw new Error(`Unsupported operator: ${operator}`);
+  }
+  return [question, correctAnswer.toString()];
+};
 
 const startCalcGame = () => runGameEngine(description, generateRound);
 
--- src/games/even.js
+++ src/games/even.js
@@ -5,11 +5,11 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
-    const randomNumber = Math.floor(Math.random() * 101);
-    const question = randomNumber;
-    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
-    return [question, correctAnswer];
-}
+  const randomNumber = Math.floor(Math.random() * 101);
+  const question = randomNumber;
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
 
 const startEvenGame = () => runGameEngine(description, generateRound);
 
